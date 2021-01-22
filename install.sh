9e5751c7af7348eb2ff5ca23e125d6df9b458b15
Hao-Chun Peng <pworker.tw@gmail.com>
2021-01-22 22:32:55 +0800

    update usage

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
-
 ln -sf dotfiles/_vimrc ~/.vimrc
 ln -sf dotfiles/_vim ~/.vim
 ln -sf dotfiles/_tmux.conf ~/.tmux.conf
 ln -sf dotfiles/_zshrc ~/.zshrc
 ln -sf dotfiles/_oh-my-zsh ~/.oh-my-zsh
+
+git submodule init
+git submodule update
+
+#install oh-my-zsh & plugins with submodule 
 
 ln -sf ~/dotfiles/zsh-plugins/zsh-syntax-highlighting ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 ln -sf ~/dotfiles/zsh-plugins/zsh-autosuggestions ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-autosuggestions
