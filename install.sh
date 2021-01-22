ac629bd098c5dc3c513957c07b97c09408f24768
Hao-Chun Peng <pworker.tw@gmail.com>
2021-01-22 22:09:34 +0800

    install zsh plugins by using git submodule and install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git submodule init
+git submodule update
+
+ln -sf dotfiles/_vimrc ~/.vimrc
+ln -sf dotfiles/_vim ~/.vim
+ln -sf dotfiles/_tmux.conf ~/.tmux.conf
+ln -sf dotfiles/_zshrc ~/.zshrc
+ln -sf dotfiles/_oh-my-zsh ~/.oh-my-zsh
+
+ln -sf ~/dotfiles/zsh-plugins/zsh-syntax-highlighting ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+ln -sf ~/dotfiles/zsh-plugins/zsh-autosuggestions ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-autosuggestions
+ln -sf ~/dotfiles/zsh-plugins/zsh-completions ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-completions
