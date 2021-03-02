4152c1aaf4f6a74666b9392c951b29d72f935907
Hao-Chun Peng <pworker.tw@gmail.com>
2021-03-02 09:00:51 +0000

    update submodule of ohmyzsh

--- install.sh
+++ install.sh
@@ -4,13 +4,13 @@ ln -sf dotfiles/_vimrc ~/.vimrc
 ln -sf dotfiles/_vim ~/.vim
 ln -sf dotfiles/_tmux.conf ~/.tmux.conf
 ln -sf dotfiles/_zshrc ~/.zshrc
-ln -sf dotfiles/_oh-my-zsh ~/.oh-my-zsh
+ln -sf dotfiles/ohmyzsh ~/.ohmyzsh
 
 git submodule init
 git submodule update
 
 #install oh-my-zsh & plugins with submodule 
 
-ln -sf ~/dotfiles/zsh-plugins/zsh-syntax-highlighting ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-ln -sf ~/dotfiles/zsh-plugins/zsh-autosuggestions ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-autosuggestions
-ln -sf ~/dotfiles/zsh-plugins/zsh-completions ~/dotfiles/_oh-my-zsh/custom/plugins/zsh-completions
+ln -sf ~/dotfiles/zsh-plugins/zsh-syntax-highlighting ~/dotfiles/ohmyzsh/custom/plugins/zsh-syntax-highlighting
+ln -sf ~/dotfiles/zsh-plugins/zsh-autosuggestions ~/dotfiles/ohmyzsh/custom/plugins/zsh-autosuggestions
+ln -sf ~/dotfiles/zsh-plugins/zsh-completions ~/dotfiles/ohmyzsh/custom/plugins/zsh-completions
