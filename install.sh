ad6c2814d5c84ebe496f292de807ff46adbee31c
Hao-Chun Peng <pworker@gmail.com>
2021-08-15 22:44:21 +0800

    pyenv shell environment

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf dotfiles/_vimrc ~/.vimrc
 ln -sf dotfiles/_vim ~/.vim
 ln -sf dotfiles/_tmux.conf ~/.tmux.conf
 ln -sf dotfiles/_zshrc ~/.zshrc
+ln -sf dotfiles/_zprofile ~/.zprofile
 ln -sf dotfiles/_oh-my-zsh ~/.oh-my-zsh
 
 git submodule init
